d775866810fff26c85454406a311ad6b843bafdf
KimuraRafael <rafaelkimura57@gmail.com>
2026-02-23 07:42:59 -0300

    feature: Adicionando Controller UI para começar o front do sistema

---
+++ SetupFinder/src/main/java/com/dev/kimura/SetupFinder/Controller/SetupControllerUI.java
@@ -0,0 +1,32 @@
+package com.dev.kimura.SetupFinder.Controller;
+
+import com.dev.kimura.SetupFinder.Model.SetupItemDTO;
+import com.dev.kimura.SetupFinder.Service.SetupItemService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class SetupControllerUI {
+
+    private final SetupItemService setupItemService;
+
+    SetupControllerUI (SetupItemService setupItemService){
+
+        this.setupItemService = setupItemService;
+
+    }
+
+    @GetMapping("/listarComponentes")
+    public String listarComponentes(Model model){
+
+        List<SetupItemDTO> listarComponentes = setupItemService.listarComponentes();
+
+    }
+
+
+
+}
